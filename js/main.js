fce7ef2c128df31df88508af9831146dc8ed7d95
minhtanLT123 <111tan777@gmail.com>
2025-09-25 22:14:37 +0700

    Hoàn thành

--- js/main.js
+++ js/main.js
@@ -3,21 +3,19 @@ let arrayA = [];
 
 
 
+
 const taoMang = () => {
     if (createArraySelect.value === "Mảng có sẵn") {
         arrayA = [-3, -2, -1, 0, 1, 7, 8, 9, 10, 2, 3, 4, 5, 6]
         // arrayA = [-3, 0, -2, 7]
         document.getElementById("arrayInputShow").innerHTML = `Mảng A =  [${arrayA}] `;
         document.getElementById("arrayInputShow").classList.remove("hidden");
-        document.getElementById("arrayInput").classList.add("hidden");
-        document.getElementById("btnAddArray").classList.add("hidden");
 
     }
     else if (createArraySelect.value === "Tạo mảng mới") {
+        let arrayA = []
         document.getElementById("arrayInputShow").classList.add("hidden");
-        document.getElementById("arrayInput").classList.remove("hidden");
-        document.getElementById("btnAddArray").classList.remove("hidden");
-
+        document.getElementById("divarrayInput").classList.remove("hidden");
     }
 
 }
@@ -30,7 +28,6 @@ function addArray() {
     document.getElementById("arrayInputShow").innerHTML = `Mảng B =  [${arrayA}] `;
 }
 
-
 //1. tổng các số dương trong mảng
 document.getElementById("tongsoduong").onclick = function () {
 
@@ -38,10 +35,10 @@ document.getElementById("tongsoduong").onclick = function () {
 
     for (i = 0; i < arrayA.length; i++) {
         if (arrayA[i] > 0) {
-            tongSoDuong += arrayA[i];
+            tongSoDuong += arrayA[i] * 1;
         }
     }
-    document.getElementById("kequa1").innerHTML = tongSoDuong;
+    document.getElementById("kequa1").innerHTML = `=> ${tongSoDuong}`;
 }
 
 //2. đếm có ba nhiêu số dương trong mảng
@@ -53,7 +50,7 @@ document.getElementById("demsoduong").onclick = function () {
             dem += 1;
         }
     }
-    document.getElementById("kequa2").innerHTML = dem;
+    document.getElementById("kequa2").innerHTML = `=> ${dem}`;
 }
 //3.  tìm số nhỏ nhất trong mảng
 document.getElementById("timsonhonhat").onclick = function () {
@@ -64,7 +61,7 @@ document.getElementById("timsonhonhat").onclick = function () {
             min = arrayA[i];
         }
     }
-    document.getElementById("kequa3").innerHTML = min;
+    document.getElementById("kequa3").innerHTML = `=> ${min}`;
 }
 // 4. tìm số dương nhỏ nhất
 document.getElementById("timsoduongmin").onclick = function () {
@@ -83,7 +80,7 @@ document.getElementById("timsoduongmin").onclick = function () {
     }
 
 
-    document.getElementById("kequa4").innerHTML = min;
+    document.getElementById("kequa4").innerHTML = `=> ${min}`;
 }
 //5. Tìm số chẵn cuối cùng trong mảng. Nếu mảng không có giá trị chẵn thì trả về -1.
 document.getElementById("timchancuoi").onclick = function () {
@@ -102,7 +99,7 @@ document.getElementById("timchancuoi").onclick = function () {
     if (chan === arrayA[0] && arrayA[0] % 2 !== 0) {
         chan = -1;
     }
-    document.getElementById("kequa5").innerHTML = chan;
+    document.getElementById("kequa5").innerHTML = `=> ${chan}`;
 }
 
 // 6. Đổi chỗ 2 giá trị trong mảng theo vị trí (Cho nhập vào 2 vị trí muốn đổi chỗ giá trị).
@@ -114,9 +111,6 @@ document.getElementById("doiso").onclick = function () {
     let tempA = arrayA[viTri1];
     arrayA[viTri1] = arrayA[viTri2];
     arrayA[viTri2] = tempA;
-
-
-
 
     document.getElementById("kequa6").innerHTML = `=> Mảng sau khi hoán đổi: ${arrayA}`;
     console.log(viTri1);
@@ -139,7 +133,7 @@ document.getElementById("tangdan").onclick = function () {
     }
     newArrayA = arrayA;
 
-    document.getElementById("kequa7").innerHTML = `[${newArrayA}]`;
+    document.getElementById("kequa7").innerHTML = `=>[${newArrayA}]`;
 }
 
 //8. Tìm số nguyên tố đầu tiên trong mảng. Nếu mảng không có số nguyên tố thì trả về – 1.
@@ -164,8 +158,7 @@ document.getElementById("songuyento").onclick = function () {
             break;
         }
     }
-
-    document.getElementById("kequa8").innerHTML = result;
+    document.getElementById("kequa8").innerHTML = `=> ${result}`;
 }
 
 //9. Nhập thêm 1 mảng số thực, tìm xem trong mảng có bao nhiêu số nguyên?
@@ -174,10 +167,10 @@ function addArray2() {
 
     const arrayInput = document.getElementById("arrayInput2").value;
     arrayC.push(arrayInput);
-    document.getElementById("arrayInputShow2").innerHTML = ` Mảng C = [${arrayC}]`;
+    document.getElementById("arrayInput2").value = "";
+    document.getElementById("arrayInputShow2").innerHTML = `  => Mảng C = [${arrayC}]`;
 
 }
-
 document.getElementById("songuyen").onclick = function () {
     let dem = 0;
     let soNguyen = 0;
